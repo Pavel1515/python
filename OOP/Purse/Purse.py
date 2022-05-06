986840fe05fa98986c641e4779ca91ba12987682
Pavlo Yerebakan <ppvr3407@gmail.com>
2022-05-06 23:25:08 +0300

    Розібрався чучуть

--- OOP/Purse/Purse.py
+++ OOP/Purse/Purse.py
@@ -1,20 +1,21 @@
 class Purse():
 	def __init__(self,valuta,course,name = "Uknow"):
-		self.__money = 0.00
+		self._money = 0.00
 		self.valuta  = valuta
 		self.course = course
 		self.name = name
 
+
 	def info(self):
-		print(self.course , self.__money,self.valuta,self.name)
+		print(self.course , self._money,self.valuta,self.name)
 
 	def top_up_balance(self,howmany):
-		self.__money = self.__money + howmany
+		self._money = self._money + howmany
 
 	def top_down_balance(self,howmany):
-		if self.__money - howmany < 0 :
+		if self._money - howmany < 0 :
 			print("Не достаточно средств")
 		else:
-			self.__money = self.__money - howmany
+			self._money = self.money - howmany
 	def transfer(self):
-		self.__money * self.course
+		self._money * self.course
--- OOP/Purse/Purse_v2.py
+++ OOP/Purse/Purse_v2.py
@@ -2,13 +2,11 @@ from Purse import Purse
 
 class V2(Purse):
     def save(self):
-        with open("money.txt", "a") as r:
-            r.write(str(self.money) +"\n")
-    def __init__(self,valuta,course,name="Uknow"):
-        Purse.__init__(self,valuta,course,name)
-
+        with open("money.txt" , "a") as r:
+            r.write(self.name + " " +str(self._money) + "\n")
 
 
 c  = V2("USD", 9,'Pavlo')
 c.top_up_balance(1000) 
 c.info()
+c.save()
