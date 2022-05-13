8b8b34033eed07276d57cc3bdaa9072df2eafd86
Pavlo Yerebakan <ppvr3407@gmail.com>
2022-05-14 00:42:52 +0300

    наконецто доделал

--- Telegram_bot_2/app.py
+++ Telegram_bot_2/app.py
@@ -3,27 +3,39 @@ from telebot import types,telebot
 from googletrans import Translator
     
 bot = telebot.TeleBot(token)
+list_en = ['b','d','f','g','h','j','k','l','m','n','q','r','s','t','v','w','z']
 
 @bot.message_handler(content_types = ["text"])
 def handler(message):
-    list_en = ['b','d','f','g','h','j','k','l','m','n','q','r','s','t','v','w','x','z']
-    for z in message.text.lower():
-        for i in list_en:
-            if z == i:
-                Ru_translate(message)
-            else:
-                Engl_translate(message)
-            break
+    y = message.text
+    b = False
+    try:
+        for z in str(y.lower()):
+            for i in list_en:
+                if i == z :
+                    Ru_translate(message)
+                    m = 10
+                    b = True
+                    break
+            if b == True:
+                break
+        print(m)
+    except:
+        Engl_translate(message)
 
-def Ru_translate(message): 
-    trans = Translator()
-    t = trans.translate(message.text)
-    bot.send_message(message.chat.id, t.text)
-             
+
+    
+@bot.message_handler(content_types = ["text"])             
 def Engl_translate(message):
     trans = Translator()
-    t = trans.translate(message.text ,dest = 'en')
+    t = trans.translate(message.text,src = 'ru', dest='en')
     bot.send_message(message.chat.id, t.text)
 
+@bot.message_handler(content_types = ["text"]) 
+def Ru_translate(message): 
+    trans = Translator()
+    t = trans.translate(message.text,dest ='ru')
+    bot.send_message(message.chat.id, t.text)
+    
 if __name__ == '__main__':
      bot.polling()
